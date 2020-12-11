8abd7a9903d581ae66c6f2223ed1b1b0905cacfa
seanphenomenon <seanvharrell@gmail.com>
2020-12-11 02:09:18 -0800

    added footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="footer" style={{ backgroundColor: "#343a40" }}>
+    <footer className="footer-container" style={{ backgroundColor: "#343a40" }}>
       <div
         className="footer-text text-center py-3"
         style={{
@@ -12,7 +12,23 @@ export default function Footer() {
           letterSpacing: ".5px",
         }}
       >
-        © Copyright 2020 - Sean Harrell.
+        &#169; Copyright 2020 - Sean Harrell.
+        <div>
+          <a href="http://www.linkedin.com/in/seanharrell">
+            <i
+              className="fab fa-linkedin-in"
+              id="footer-icon"
+              style={{ fontSize: "1.3em", padding: "10px", color: "#fff" }}
+            ></i>
+          </a>
+          <a href="http://www.github.com/seanphenomenon">
+            <i
+              className="fab fa-github"
+              id="footer-icon"
+              style={{ fontSize: "1.3em", padding: "10px", color: "#fff" }}
+            ></i>
+          </a>
+        </div>
       </div>
     </footer>
   );
